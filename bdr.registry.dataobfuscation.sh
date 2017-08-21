e723a94a95913e8b111b054271579f1968b4a26b
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-08-21 16:38:05 +0300

    [refs #87513] Add env file for bdr-registry script

--- bdr.registry.dataobfuscation.sh
+++ bdr.registry.dataobfuscation.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo Importing...
-
 mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
 create database  IF NOT EXISTS bdr_copy;
 EOF
@@ -12,24 +10,23 @@ mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
 
 use bdr_copy;
 
-update bdr_registry_company set name = concat('NMORGANIZATION', id),
-                                addr_street = concat('NMSTREET', id),
-                                addr_postalcode = concat('CP', id),
-                                vat_number = concat('VAT', id),
-                                website = concat('htttp://www.x', id, '.com');
+update bdr_registry_company set name = concat('ORG', SHA1(name)),
+                                addr_street = 'Kongens Nytorv 6, 1050 Copenhagen Denmark',
+                                addr_postalcode = concat('CP', SHA1(addr_postalcode)),
+                                vat_number = 'VAT0000',
+                                website = concat('htttp://www.x', SHA1(website), '.com');
 
-update bdr_registry_companynamehistory set name = concat('NMORGANIZATION', company_id);
+update bdr_registry_companynamehistory set name = concat('ORG', SHA1(name));
 
 update bdr_registry_person set family_name = concat('LNAME', id),
-			                   first_name = concat('FNAME', id),
-			                   email = concat(id, 'EMAIL@climaOds2010,yyy'),
-			                   phone = concat(000, id, 000),
-                               fax = concat(000, id , 000);
+			                   first_name = concat('FNAME', SHA1(first_name)),
+			                   email = concat(SHA1(email), 'EMAIL@climaOds2010.yyy'),
+			                   phone = concat(000, SHA1(phone), 000),
+                               fax = concat(000, SHA1(fax) , 000);
 
 update bdr_registry_comment set text = '';
 
-update bdr_registry_account set uid = concat('uid', id),
-				password = concat('pass', id);
+update bdr_registry_account set password = '${PASSWORD}';
 
 drop database $DB_NAME;
 
